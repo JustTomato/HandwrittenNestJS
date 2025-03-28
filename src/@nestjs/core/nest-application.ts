ffd3c91eb08c62f2494377807630a2469248d07f
Sapphire94 <876316412@qq.com>
2025-03-28 20:28:40 +0800

    8.获取响应

--- src/@nestjs/core/nest-application.ts
+++ src/@nestjs/core/nest-application.ts
@@ -4,7 +4,10 @@ import path from 'path'
 import 'reflect-metadata'
 export class NestApplication {
   private readonly app: Express = express();
-  constructor(protected readonly module) { }
+  constructor(protected readonly module) {
+    this.app.use(express.json()) // 用于把json格式的请求体对象放在req.body上
+    this.app.use(express.urlencoded({ extended: true })) //把form表单格式的请求体对象放在req.body上
+  }
   use(middleware) {
     this.app.use(middleware);
   }
@@ -26,12 +29,21 @@ export class NestApplication {
         this.app[httpMethod.toLowerCase()](routePath, (req: ExpressRequest, res: ExpressResponse, next: NextFunction) => {
           const args = this.resolveParams(controller, methodName, req, res, next)
           const result = method.call(controllers, ...args);
-          res.send(result);
+          // 判断controller的methodName方法里有没有使用Response或者Res参数装饰器，如果使用了其中一个则不给客户端发送响应
+          const responseMetadata = this.getResponseMetadata(controller, methodName);
+          if (!responseMetadata || responseMetadata?.data?.passthrough) {
+            // 把返回值序列化返回给客户端
+            res.send(result);
+          }
         })
         Logger.log(`Mapped {${routePath}, ${httpMethod}} route`, 'RoutesResolver');
       }
     }
     Logger.log(`Nest application successfully started`, 'NestApplication')
+  }
+  private getResponseMetadata(controller, methodName) {
+    const paramsMetadata = Reflect.getMetadata('params', controller, methodName) ?? [];
+    return paramsMetadata.filter(Boolean).find((param) => param.key === 'Response' || param.key === 'Res')
   }
   private resolveParams(instance: any, methodName: string, req: ExpressRequest, res: ExpressResponse, next: NextFunction) {
     const paramsMetadata = Reflect.getMetadata('params', instance, methodName) ?? [];
@@ -47,11 +59,16 @@ export class NestApplication {
         case 'Headers':
           return data ? req.headers[data] : req.headers
         case 'Session':
-          return req;//youwenti 
+          return req; //has problem 
         case 'Ip':
           return req.ip
         case 'Param':
           return data ? req.params[data] : req.params;
+        case 'Body':
+          return data ? req.body[data] : req.body;
+        case 'Response':
+        case 'Res':
+          return res;
         default:
           return null
       }
--- src/user.controller.ts
+++ src/user.controller.ts
@@ -1,5 +1,9 @@
-import { Controller, Get, Req, Request, Query, Headers, Session, Ip, Param } from "@nestjs/common";
-import { Request as ExpressRequest } from 'express'
+import {
+  Controller,
+  Get, Post,
+  Req, Request, Query, Headers, Session, Ip, Param, Body, Response
+} from "@nestjs/common";
+import { Request as ExpressRequest, Response as ExpressResponse } from 'express'
 @Controller('users')
 export class UserController {
   @Get('req')
@@ -46,4 +50,37 @@ export class UserController {
   handleWildCard() {
     return 'handleWildCard'
   }
+  @Post('create')
+  createUser(@Body() createUserDto, @Body('username') username: string) {
+    console.log('createUserDto', createUserDto)
+    console.log('username', username)
+    return 'user created'
+  }
+  @Get('response')
+  response(@Response() response: ExpressResponse) {
+    console.log('response', response)
+    response.send('send');
+    // response.json({ success: true })
+    return 'response'
+  }
+  @Get('passthrough')
+  passthrough(@Response({ passthrough: true }) response: ExpressResponse) {
+    response.setHeader('key', 'value')
+    return 'response'
+  }
 }
+/**
+ * 在使用Nest.js的时候，一般来说一个实体会定义两个类型，一个是dto(data transform object)，一个是interface
+ * dto 客户端向服务器提交数据对象，比如说当用户注册的时候（用户名跟密码）然后服务器一般会获取此dto，然后保存在数据库中
+ * 保存的时候可能还会加入其他一些默认值例如时间戳、对密码加密
+ * 同时也还会过滤掉某些字段，比如注册的时候密码跟确认密码，但是保存到数据库时只需要密码
+ * 
+ * 数据库保存的数据类型一般会定义为一个interface
+ * 
+ * 例如
+ * 
+ * userDto { 用户名 密码 确认密码 }
+ * 
+ * userInterface { 用户名、密码、创建时间、 更新时间 }
+ * 
+ */
